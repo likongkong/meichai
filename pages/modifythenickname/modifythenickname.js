fcd09f55169a19fc53c627214f4e44a4eda76ea4
likongkong <li10145210@163.com>
2020-10-29 17:14:14 +0800

    万圣活动

--- pages/modifythenickname/modifythenickname.js
+++ pages/modifythenickname/modifythenickname.js
@@ -15,19 +15,75 @@ Page({
     tgabox:false,
     loginid: app.signindata.loginid,
     uid: app.signindata.uid,
-
-
+    dateTime:0,
+    issharePopup:false,
+    ishelpPopup:false,
+    canAssist:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options)
+    this.showLeft();
+    // this.getData();
     // 判断是否授权
     this.activsign();
   },
   onLoadfun:function(){
-
+    // '已经授权'
+    this.setData({
+      loginid: app.signindata.loginid,
+      uid: app.signindata.uid
+    });
+    this.getData();
+  },
+  getData(){
+    var _this = this;
+    wx.showLoading({ title: '加载中...', }) 
+    var q = Dec.Aese('mod=festival&operation=listWSJ&uid=' +_this.data.uid+'&loginid='+_this.data.loginid+'&shareUId='+_this.data.shareUId+'&shareDate='+_this.data.shareDate);
+    console.log('mod=festival&operation=listWSJ&uid=' +_this.data.uid+'&loginid='+_this.data.loginid)
+    wx.request({
+      url: app.signindata.comurl + 'spread.php' + q,
+      method: 'GET',
+      header: { 'Accept': 'application/json' },
+      success: function (res) { 
+        if (res.data.ReturnCode == 200) {
+          for(let i=0;i<res.data.List.date.length;i++){
+            res.data.List.date[i] = _this.formatTime(res.data.List.date[i])
+          }
+          _this.setData({
+            datetimeAll:res.data.List.date,
+            rank:res.data.List.rank,
+            tips:res.data.Info.tips,
+            share:res.data.List.share,
+            canAssist:res.data.Info.canAssist
+          })
+        }
+      },
+      fail: function () {},
+      complete:function(){
+        wx.hideLoading()
+      }
+    });
+  },
+  //时间戳转换成日期时间
+  formatTime(time) {
+    var date = new Date(time*1000);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    // return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;//年月日时分秒
+    return m + '月' + d + '日';
   },
   activsign: function () {
     // 判断是否授权 
@@ -105,54 +161,64 @@ Page({
     });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+ 
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // onPullDownRefresh: function () {
+  
+  // },
+  // onReachBottom: function () {
 
-  },
+  // },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
   clicktganone: function () {
     this.setData({ tgabox: false })
   }, 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+ 
+  tabBtnFun(e){
+    this.setData({
+      dateTime:e.currentTarget.dataset.datetime
+    })
+  },
+  showLeft() {
+    //获取当前时间
+    var nowTime = Date.now();
+    // var nowTime = new Date().getTime();
+    //获取当天 23:59 
+    var endTime = new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1
+    //获取时间差
+    var timediff = Math.round((endTime - nowTime) / 1000);
+    //获取还剩多少小时
+    var hour = parseInt(timediff / 3600 % 24) + parseInt(timediff / 3600 / 24) * 24;
+    //获取还剩多少分钟
+    var minute = parseInt(timediff / 60 % 60);
+    //获取还剩多少秒
+    var second = timediff % 60;
+    //输出还剩多少时间
+    hour = this.timerFilter(hour);
+    minute = this.timerFilter(minute);
+    second = this.timerFilter(second);
+    // console.log(hour + "时" + minute + "分" + second + "秒")
+    this.setData({
+      todaycountdown:hour + ":" + minute + ":" + second
+    })
+    setTimeout(()=>{
+      this.showLeft()
+    },1000)
+  },
+  //给小于10的数值前面添加 0 
+  timerFilter(params) {
+      if (params - 0 < 10) {
+          return '0' + params
+      } else {
+          return params
+      }
+  },
+  showSharePopup(){
+    this.setData({ issharePopup: !this.data.issharePopup })
+  },
+  showhelpPopup(){
+    this.setData({ ishelpPopup: !this.data.ishelpPopup })
   }
+
 })
