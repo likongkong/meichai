54225ab293b90d020cb4e73cfb94e536fba9b07e
likongkong <li10145210@163.com>
2020-10-30 18:50:10 +0800

    修改获取当前日期方法

--- pages/modifythenickname/modifythenickname.js
+++ pages/modifythenickname/modifythenickname.js
@@ -224,8 +224,16 @@ hideModal: function () {
   },
   getData(){
     var _this = this;
-    var myDate = new Date();
-    var time = myDate.toLocaleDateString().split('/').join('');
+    var timestamp = Date.parse(new Date());
+    var date = new Date(timestamp);
+    //获取年份  
+    var Y =date.getFullYear();
+    //获取月份  
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+    //获取当日日期 
+    var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(); 
+    var time = Y +''+ M +''+ D;
+    console.log('time=======111111111',time)
     if( time == 20201030){
       _this.setData({
         dateTime : 0,
