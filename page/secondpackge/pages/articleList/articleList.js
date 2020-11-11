f44baf0e98bc1cc03bf6f36c6ff4ff571d1fd8e3
likongkong <li10145210@163.com>
2020-11-11 18:19:02 +0800

    修改文章列表页下拉刷新

--- page/secondpackge/pages/articleList/articleList.js
+++ page/secondpackge/pages/articleList/articleList.js
@@ -158,7 +158,8 @@ Page({
       },
       fail: function () {},
       complete:function(){
-        wx.hideLoading()
+        wx.hideLoading();
+        wx.stopPullDownRefresh();
       }
     });
   },
@@ -231,7 +232,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    _this.getData();
   },
 
   /**
