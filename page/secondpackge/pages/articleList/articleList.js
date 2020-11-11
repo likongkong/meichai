53e712ecba22c9a755646bb0c3b22c10e0a65d8a
likongkong <li10145210@163.com>
2020-11-11 18:15:38 +0800

    新增文章列表页

---
+++ page/secondpackge/pages/articleList/articleList.js
@@ -0,0 +1,250 @@
+var Dec = require('../../../../common/public.js'); //aes加密解密js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    c_title: '美拆', 
+    c_arrow: true,
+    c_backcolor: '#ff2742',
+    statusBarHeightMc: wx.getStorageSync('statusBarHeightMc'),
+    windowHeight: app.signindata.windowHeight - wx.getStorageSync('statusBarHeightMc')||0,
+    signinlayer: true,
+    tgabox:false,
+    loginid: app.signindata.loginid,
+    uid: app.signindata.uid,
+    listData:[]
+  },
+
+/**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('options===',options)
+    this.onLoadfun();
+    // 判断是否授权
+    // if(app.signindata.sceneValue==1154){
+    //   app.signindata.isProduce = true;  
+    //   this.onLoadfun();
+    // }else{
+    //   this.activsign();
+    // };
+    // this.activsign();
+  },
+  activsign: function () {
+    // 判断是否授权 
+    var _this = this;
+    // 判断是否登录
+    if (_this.data.loginid != '' && _this.data.uid != '') {
+      _this.onLoadfun();
+      return false;
+    };    
+
+    if(app.signindata.sceneValue==1154){
+      app.signindata.isProduce = true;  
+      _this.onLoadfun();
+    }else{
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // '已经授权'
+            _this.setData({
+              loginid: app.signindata.loginid,
+              uid: app.signindata.uid,
+              openid: app.signindata.openid,
+              avatarUrl: app.signindata.avatarUrl,
+              isShareFun: app.signindata.isShareFun,
+              isProduce: app.signindata.isProduce,
+              signinlayer: true,
+              tgabox: false
+            });
+            // 判断是否登录
+            if (_this.data.loginid != '' && _this.data.uid != '') {
+              _this.onLoadfun();
+            } else {
+              app.signin(_this);
+            }
+          } else {
+            _this.setData({
+              tgabox: false,
+              signinlayer: false
+            })
+            // '没有授权 统计'
+            app.userstatistics(43);
+            _this.onLoadfun();
+          }
+        }
+      });  
+    };    
+  },
+  // 授权点击统计
+  clicktga: function () {
+    app.clicktga(2)
+  },
+  clicktganone: function () {
+    this.setData({ tgabox: false })
+  },
+  // 点击登录获取权限
+  userInfoHandler: function (e) {
+    var _this = this;
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          _this.setData({
+            signinlayer: true,
+            tgabox: false
+          });
+          _this.activsign();
+          // 确认授权用户统计
+          app.clicktga(4);          
+        }
+      }
+    });
+    if (e.detail.detail.userInfo) { } else {
+      app.clicktga(8)  //用户按了拒绝按钮
+      
+    };
+  },
+  pullupsignin: function () {
+    // // '没有授权'
+    this.setData({
+      tgabox: true
+    });
+  },
+
+
+  onLoadfun:function(){
+    // '已经授权'
+    this.setData({
+      loginid: app.signindata.loginid,
+      uid: app.signindata.uid
+    });
+    this.getData();
+  },
+
+  getData(){
+    var _this = this;
+    wx.showLoading({ title: '加载中...', }) 
+
+    wx.request({
+      url: 'https://meichai-1300990269.cos.ap-beijing.myqcloud.com/produce/articleList.json?2020',
+      method: 'GET',
+      header: { 'Accept': 'application/json' },
+      success: function (res) { 
+        if(res.data[0].litpic_type == 1){
+          res.data[0].first = 1;
+        }
+        console.log('getData====',res.data)
+        console.log('getData====',res)
+        let arr1 =[];
+        for(let i=0;i<res.data.length;i++){
+          let arr = [];
+          for(let k=0;k<res.data.length;k++){
+            if(res.data[k].publish_time == res.data[i].publish_time){
+              arr.push(res.data[k])
+            }
+          }
+          arr1.push({list:arr,publish_time:_this.formatTime(res.data[i].publish_time)})
+        }
+        var listarr = _this.distinct(arr1);
+
+        console.log(listarr)
+        _this.setData({
+          listData:listarr,
+          listDataLen:res.data.length
+        })
+      },
+      fail: function () {},
+      complete:function(){
+        wx.hideLoading()
+      }
+    });
+  },
+ 
+  //  数组去重
+  distinct:function(arr){
+    var arr = arr,i,j,len = arr.length;
+    for(i = 0; i < len; i++){
+        for(j = i + 1; j < len; j++){
+          if (arr[i].publish_time == arr[j].publish_time){
+              arr.splice(j,1);
+              len--;
+              j--;
+          }
+        }
+    }
+    return arr;
+  },
+
+  //时间戳转换成日期时间
+  formatTime(time) {
+    var date = new Date(time*1000);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    // return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;//年月日时分秒
+    return m + '月' + d + '日';
+  },
+  jumpWebview(e){
+    let url = e.currentTarget.dataset.uel;
+    app.comjumpwxnav(0,url,'','')
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
