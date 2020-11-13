e03c861adbcf0de82bfd547502322d23a00b242c
wei <www93111@163.com>
2020-11-13 15:46:03 +0800

    添加抽签新页面

---
+++ page/component/pages/newLimitlottery/newLimitlottery.js
@@ -0,0 +1,198 @@
+var Dec = require('../../../../common/public.js'); //aes加密解密js
+const app = getApp();
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    c_title: '限定抽签',
+    c_arrow: true,
+    c_backcolor: '#ff2742',
+    statusBarHeightMc: wx.getStorageSync('statusBarHeightMc')|| 90,
+    // 授权弹框
+    tgabox: false,
+    signinlayer:true,
+    windowHeight: app.signindata.windowHeight - 65 - wx.getStorageSync('statusBarHeightMc') || 0,
+
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // 判断是否授权 
+    var _this = this;
+    _this.setData({
+      loginid: app.signindata.loginid,
+      uid: app.signindata.uid,
+      openid: app.signindata.openid,
+      isProduce: app.signindata.isProduce,
+      // 适配苹果X 
+      isIphoneX: app.signindata.isIphoneX
+    });
+    if(app.signindata.sceneValue==1154){
+      app.signindata.isProduce = true;  
+      _this.onLoadfun();
+      }else{
+        wx.getSetting({
+          success: res => {
+            if (res.authSetting['scope.userInfo']) {
+              // '已经授权'
+              _this.setData({
+                loginid: app.signindata.loginid,
+                uid: app.signindata.uid,
+                openid: app.signindata.openid,
+                isProduce: app.signindata.isProduce,
+                // 适配苹果X 
+                isIphoneX: app.signindata.isIphoneX
+              });
+              // 判断是否登录
+              if (_this.data.loginid != '' && _this.data.uid != '') {
+                _this.onLoadfun();
+              } else {
+                app.signin(_this)
+              }
+              _this.setData({
+                signinlayer: true,
+              })
+            } else {
+              wx.hideLoading()
+              _this.onLoadfun();
+              this.setData({
+                signinlayer: false,
+              })
+            }
+          }
+        });
+      };
+  },
+  onLoadfun: function() {
+    var _this = this;
+    _this.setData({
+      loginid: app.signindata.loginid,
+      uid: app.signindata.uid,
+      openid: app.signindata.openid,
+      isProduce: app.signindata.isProduce,
+      // 适配苹果X 
+      isIphoneX: app.signindata.isIphoneX
+    });
+
+    // _this.listdata(1),
+
+
+
+  },
+
+
+
+  pullupsignin: function () {
+    // // '没有授权'
+    this.setData({
+      tgabox: true
+    });
+  },
+  // 授权点击统计
+  clicktga: function () {
+    app.clicktga(2)
+  },
+  clicktganone: function () {
+    this.setData({
+      tgabox: false
+    })
+  },
+  userInfoHandler: function (e) {
+    // 判断是否授权 
+    var _this = this;
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 确认授权用户统计
+          app.clicktga(4);
+          _this.setData({
+            tgabox: false,
+            signinlayer: true,
+          });
+          // '已经授权'
+          _this.data.loginid = app.signindata.loginid,
+            _this.data.openid = app.signindata.openid,
+            _this.data.isNewer = app.signindata.isNewer;
+
+          _this.setData({
+            uid: app.signindata.uid,
+            avatarUrl: app.signindata.avatarUrl,
+            isProduce: app.signindata.isProduce,
+          });
+          // 判断是否登录
+          if (_this.data.loginid != '' && _this.data.uid != '') {
+            _this.onLoadfun();
+          } else {
+            app.signin(_this);
+          };
+        } else {
+          _this.setData({
+            tgabox: true
+          });
+        }
+      }
+    });
+    if (e.detail.detail.userInfo) { } else {
+      app.clicktga(8) //用户按了拒绝按钮
+    };
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
