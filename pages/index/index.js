79e2c5d12ef1f1214f56eaa1d69e28e76614195a
likongkong <li10145210@163.com>
2020-12-02 16:03:43 +0800

    我参与的投票全部

--- pages/index/index.js
+++ pages/index/index.js
@@ -227,11 +227,11 @@ Page({
       // url: "/pages/modifythenickname/modifythenickname"
 
       // url: "/page/secondpackge/pages/articleList/articleList"
-      url: "/page/secondpackge/pages/calendarList/calendarList"
+      // url: "/page/secondpackge/pages/calendarList/calendarList"
       // url: "/pages/sputforward/sputforward"
       // url: "/page/component/pages/limitlottery/limitlottery?id=201425",
 
-      // url: "/page/component/pages/iWasInvolved/iWasInvolved",
+      url: "/page/component/pages/iWasInvolved/iWasInvolved",
 
 
 
