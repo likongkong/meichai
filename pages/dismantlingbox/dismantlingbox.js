1b5afe0f0183c9b1dfe7f184e17df9b3efe69d3e
wei <www93111@163.com>
2020-12-22 22:04:20 +0800

    展会订阅调整位置

--- pages/dismantlingbox/dismantlingbox.js
+++ pages/dismantlingbox/dismantlingbox.js
@@ -46,8 +46,8 @@ Page({
       {name:'购票',tid:1},
       {name:'线上商品',tid:2},
       {name:'线下商品',tid:3},
-      {name:'参展品牌',tid:4},
-      {name:'场馆地图',tid:5}
+      {name:'场馆地图',tid:5},
+      {name:'参展品牌',tid:4}
     ],
     // 获取手机号弹框
     havephoneiftr:false,
