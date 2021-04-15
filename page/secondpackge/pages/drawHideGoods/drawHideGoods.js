b8ba41b406467565b8b8ce75dde40073ddfe34a9
likongkong <li10145210@163.com>
2021-04-15 11:34:42 +0800

    修改抽隐藏登录

--- page/secondpackge/pages/drawHideGoods/drawHideGoods.js
+++ page/secondpackge/pages/drawHideGoods/drawHideGoods.js
@@ -58,7 +58,8 @@ Page({
     };    
     wx.getSetting({
       success: res => {
-        if (true) {
+        if (_this.data.loginid != '' && _this.data.uid != '') {
+          console.log(1111111111)
           // '已经授权'
           _this.setData({
             loginid: app.signindata.loginid,
@@ -102,19 +103,38 @@ Page({
     wx.getSetting({
       success: res => {
         if (true) {
+          // '已经授权'
           _this.setData({
+            loginid: app.signindata.loginid,
+            uid: app.signindata.uid,
+            openid: app.signindata.openid,
+            avatarUrl: app.signindata.avatarUrl,
+            isShareFun: app.signindata.isShareFun,
+            isProduce: app.signindata.isProduce,
             signinlayer: true,
             tgabox: false
           });
-          _this.activsign();
-          // 确认授权用户统计
-          app.clicktga(4);          
+          // 判断是否登录
+          if (_this.data.loginid != '' && _this.data.uid != '') {
+            _this.onLoadfun();
+          } else {
+            app.signin(_this);
+          }      
         }
       }
     });
     if (e.detail.detail.userInfo) { } else {
       app.clicktga(8)  //用户按了拒绝按钮
     };
+  },
+  userProfile(){
+    _this.setData({
+      signinlayer: true,
+      tgabox: false
+    });
+    _this.activsign();
+    // 确认授权用户统计
+    app.clicktga(4);
   },
   pullupsignin: function () {
     // // '没有授权'
--- pages/tgacomponent/tgacomponent.js
+++ pages/tgacomponent/tgacomponent.js
@@ -36,11 +36,18 @@ Component({
       const myEventDetail = e;
       this.triggerEvent("userinfo", myEventDetail);
     },
+    userProfile:function(e){
+      const myEventDetail = e;
+      wx.getUserProfile({
+        desc: '获取你的昵称、头像、地区及性别', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+           this.triggerEvent("userProfile", myEventDetail);
+        }
+      })
+
+    },
     tgatipnone:function(){
       this.triggerEvent("tgnone");
     }
   },
-
-
-
 })
