3bfd21f3b97666ea28ad98db667cf85e4b6f0359
wei <www93111@163.com>
2021-05-27 20:52:53 +0800

    VIP入口

--- app.js
+++ app.js
@@ -209,15 +209,15 @@ App({
                         }else if(num>=10){
                           num = '0'+num.toString()
                         };       
-                        // // 接口地址  
-                        // _this.signindata.comurl = 'https://api-slb.51chaidan.com/'+num+'/';
-                        // // 发现地址
-                        // _this.signindata.clwcomurl = 'https://clw-slb.51chaidan.com/'+num+'/';
-    
-                        // 接口地址  208
-                        _this.signindata.comurl = 'https://api.51chaidan.com/';
+                        // 接口地址  
+                        _this.signindata.comurl = 'https://api-slb.51chaidan.com/'+num+'/';
                         // 发现地址
-                        _this.signindata.clwcomurl = 'https://clw.51chaidan.com/';
+                        _this.signindata.clwcomurl = 'https://clw-slb.51chaidan.com/'+num+'/';
+    
+                        // // 接口地址  208
+                        // _this.signindata.comurl = 'https://api.51chaidan.com/';
+                        // // 发现地址
+                        // _this.signindata.clwcomurl = 'https://clw.51chaidan.com/';
                       }else{
                         // 接口地址  
                         _this.signindata.comurl = 'http://api-test.51chaidan.com/';
