accd80a5381f22e7c76cfe93510d73e9d6bf0d5f
likongkong <li10145210@163.com>
2021-06-12 01:43:03 +0800

    11.9.7 邀请函修改

--- page/secondpackge/pages/priorityVoting/priorityVoting.js
+++ page/secondpackge/pages/priorityVoting/priorityVoting.js
@@ -303,11 +303,11 @@ Page({
   },
   dateFun(){
     var nowTime = new Date().getTime();
-    // var nowTime = '1623301200000';
-    var time11 = '1623384000000';
-    var time12 = '1623470400000';
-    var time13 = '1623556800000';
-    var time14 = '1623643200000';
+    // var nowTime = '1623517200000';
+    var time11 = '1623427200000';
+    var time12 = '1623513600000';
+    var time13 = '1623600000000';
+    var time14 = '1623686400000';
 
     // if(nowTime>'1623340800000'){
     //   this.data.objectArray[0].disabled = true;
@@ -327,14 +327,17 @@ Page({
       this.data.objectArray[2].disabled = true;
       this.setData({num:3})
     }else{
-      if(nowTime >= time11 && nowTime < time12){
+      if(nowTime >= time12 && nowTime <= time13){
+        console.log(2)
         this.data.objectArray[0].disabled = true;
         this.setData({num:5})
-      }else if(nowTime >= time12 && nowTime < time13){
+      }else if(nowTime >= time13 &&  nowTime <= time14){
+        console.log(3)
         this.data.objectArray[0].disabled = true;
         this.data.objectArray[1].disabled = true;
         this.setData({num:6})
-      }else if(nowTime >= time13 || nowTime >= time14){
+      }else if(nowTime >= time14){
+        console.log(4)
         this.data.objectArray[0].disabled = true;
         this.data.objectArray[1].disabled = true;
         this.data.objectArray[2].disabled = true;
