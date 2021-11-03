5cca0f966fc91d6741898e8bc485871853ff6493
wei <www93111@163.com>
2021-11-03 16:19:26 +0800

    关联

--- page/settled/pages/fieldGuideList/fieldGuideList.js
+++ page/settled/pages/fieldGuideList/fieldGuideList.js
@@ -205,6 +205,7 @@ Page({
             dataList[i].is_select = true;
             this.data.selectTitle = e.currentTarget.dataset.title;
             this.data.selectId = e.currentTarget.dataset.id;
+            this.data.relationType = dataList[i].type || '';
           };
        }else{
          dataList[i].is_select = false;
@@ -240,6 +241,7 @@ Page({
         [`fieldGuideData2[0].value`]:this.data.selectTitle,
       })
       prevPage.data.obj.associationActivity=this.data.selectId;
+      prevPage.data.obj.relationType = this.data.relationType;
     }
     console.log(prevPage.data.obj)
     wx.navigateBack({
